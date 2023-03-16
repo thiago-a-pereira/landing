7802248ca4a8af85192bb59c294adeff8dabd74f
Thiago Araújo Pereira <thiagoap12@live.com>
2023-03-16 02:01:32 -0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,28 +12,30 @@ scrollUp.addEventListener("click", () => {
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function menu() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-  var y = document.getElementById("botao");
-  if (y.className === "fa fa-bars"){
-    y.className += "fa fa-close";
-  } else {
-    y.className = "fa fa-bars";
-  }
-  var z = document.getElementById("opaco-id");
-  if (z.className === "opaco"){
-    z.className += " responsive";
-  } else{
-    z.className = "opaco"
-    z.addEventListener("click", function(){
-      z.className = "opaco";
-      y.className = "fa fa-bars";
+  if (window.innerWidth < 860) {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
       x.className = "topnav";
-    })
+    }
+    var y = document.getElementById("botao");
+    if (y.className === "fa fa-bars"){
+      y.className += "fa fa-close";
+    } else {
+      y.className = "fa fa-bars";
+    }
+    var z = document.getElementById("opaco-id");
+    if (z.className === "opaco"){
+      z.className += " responsive";
+    } else{
+      z.className = "opaco"
+      z.addEventListener("click", function(){
+        z.className = "opaco";
+        y.className = "fa fa-bars";
+        x.className = "topnav";
+      })
+    }
   }
 }
 
