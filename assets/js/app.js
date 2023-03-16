f68ed764c6afe08946c8a26dc67aca7132a9cad1
Thiago Araújo Pereira <thiagoap12@live.com>
2023-03-16 02:15:52 -0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,10 +14,13 @@ scrollUp.addEventListener("click", () => {
 function menu() {
   if (window.innerWidth < 860) {
     var x = document.getElementById("myTopnav");
+    var opa = document.getElementById("opaco-id");
     if (x.className === "topnav") {
       x.className += " responsive";
+      opa.className = "opaco responsive";
     } else {
       x.className = "topnav";
+      opa.className = "opaco";
     }
     var y = document.getElementById("botao");
     if (y.className === "fa fa-bars"){
@@ -25,17 +28,17 @@ function menu() {
     } else {
       y.className = "fa fa-bars";
     }
-    var z = document.getElementById("opaco-id");
-    if (z.className === "opaco"){
-      z.className += " responsive";
-    } else{
-      z.className = "opaco"
-      z.addEventListener("click", function(){
-        z.className = "opaco";
+  }
+
+
+  if(opa.className === "opaco responsive"){
+    opa.addEventListener("click", function(){
+      if(opa.className === "opaco responsive"){
         y.className = "fa fa-bars";
         x.className = "topnav";
-      })
-    }
+        opa.className = "opaco";
+      }
+    })
   }
 }
 
@@ -51,8 +54,6 @@ function popUp(id) {
   }
   if(opa.className === "opaco responsive"){
     opa.addEventListener("click", function(){
-      console.log("click ok");
-      console.log(pop.className);
       if(pop.className === "popuptext show"){
         pop.classList.toggle("show");
         opa.className = "opaco";
@@ -60,26 +61,3 @@ function popUp(id) {
     })
   }
 }
-
-
-
-/*function popUp(id) {
-  var pop = document.getElementById(id);
-  if (pop.className === "popuptext"){
-    pop.className += " show";
-  } else {
-    pop.className = "popuptext";
-  }
-  var opa = document.getElementById("opaco-id");
-  if (opa.className === "opaco"){
-    opa.className += " responsive";
-  } else{
-    opa.className = "opaco";
-    pop.className = "popuptext";
-    opa.addEventListener("click", function(){
-      pop.className = "popuptext";
-      opa.className = "opaco";
-    })
-  }
-}*/
-
