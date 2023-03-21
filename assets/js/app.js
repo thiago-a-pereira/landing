b8d8e8372ab51359c0dddcaa7091821fe2a32590
Thiago Araújo Pereira <thiagoap12@live.com>
2023-03-21 00:55:29 -0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,3 @@
-const scrollUp = document.querySelector("#scroll-up");
-
-scrollUp.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth",
-    });
-});
-
 
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
@@ -62,7 +52,10 @@ function popUp(id) {
   }
 }
 
-
+function loadFrame(){
+  var myFrame = document.getElementById('loc-titulo');
+  myFrame.src = "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15287.585865404106!2d-49.2585498!3d-16.6820636!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935ef165d4163bfb%3A0x7e60d70be9bf3c32!2sGon%C3%A7alves%2C%20Macedo%2C%20Paiva%20e%20Rassi%20Advogados%20-%20GMPR!5e0!3m2!1spt-BR!2sbr!4v1678639832256!5m2!1spt-BR!2sbr";
+};
 
 /*function popUp(id) {
   var pop = document.getElementById(id);
@@ -82,5 +75,18 @@ function popUp(id) {
       opa.className = "opaco";
     })
   }
-}*/
+}
+
+
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+});
+
+*/
 
